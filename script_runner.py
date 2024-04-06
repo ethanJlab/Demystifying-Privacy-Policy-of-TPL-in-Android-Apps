63c96052d3005eb76453e9c37e0d33c6966b9fdf
ethanJlab <ethanjanicki89@gmail.com>
2024-04-06 14:46:09 -0400

    Refactor script_runner.py to include new functions and print outputs

--- script_runner.py
+++ script_runner.py
@@ -1,15 +1,19 @@
 # Part 1 imports
 from Part1_dataset.Step1_1_get_TPL_list_info import tpl_statistics
-from
+from Part1_dataset.Step1_2_get_TPL_privacy_policy_files import analyze_tpl_privacy_policies
 
 # Define the path to the TPL list folder you want to analyze
 tpl_list_folder_path = "dataset/TPL_PP"
 
 # Call the tpl_statistics function with the path to your TPL list folder
 # The function will print the statistics and also return them as a string
-statistics_output = tpl_statistics(tpl_list_folder_path)
+tpl_statistics_output = tpl_statistics(tpl_list_folder_path)
+analyze_tpl_privacy_policies_output = analyze_tpl_privacy_policies(tpl_list_folder_path)
 
-# If you wish to use the returned statistics output for further processing or logging,
-# you can do so here. For demonstration, we'll just print the returned value again.
-print("Returned Statistics Output:")
-print(statistics_output)
+# Print outputs
+print("Returned tpl_statistics_output:")
+print(tpl_statistics_output)
+
+print("\nReturned analyze_tpl_privacy_policies_output:")
+print(analyze_tpl_privacy_policies_output)
+
