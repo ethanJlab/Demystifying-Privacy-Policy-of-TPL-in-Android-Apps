6eda5d2f39cdd00168eddf2dff92214d192f727a
ethanJlab <ethanjanicki89@gmail.com>
2024-04-06 14:49:20 -0400

    Refactor TPL binary files analysis

--- Part1_dataset/Step1_3_get_TPL_binaryfiles_info.py
+++ Part1_dataset/Step1_3_get_TPL_binaryfiles_info.py
@@ -1,47 +1,59 @@
-# coding: utf-8
-# Author: kaifa.zhao@connect.polyu.hk
-# Copyright 2021@Kaifa Zhao (Zachary)
-# Date: 2023/4/27
-# System: linux
-
 import os
 from Utils import check_macos_files
 
-
-def main(TPL_binaryfiles_folder):
+def analyze_tpl_binary_files(TPL_binaryfiles_folder):
+    """
+    Analyzes TPL binary files, counting distinct TPL binary files, jar, and aar files.
+    
+    :param TPL_binaryfiles_folder: The path to the folder containing TPL binary files.
+    :return: A dictionary with statistics on the TPL binary files, and counts of jar and aar files.
+    """
     statistics = {}
-    num_arr = 0
     num_jar = 0
+    num_aar = 0
+
     for TPL_cat in os.listdir(TPL_binaryfiles_folder):
-        #
         cat_folder = os.path.join(TPL_binaryfiles_folder, TPL_cat)
         if check_macos_files(TPL_cat) or os.path.isfile(cat_folder):
             continue
+
         statistics[TPL_cat] = 0
         print(TPL_cat)
+
         for TPL in os.listdir(cat_folder):
-            if check_macos_files(TPL) or TPL.startswith("."): continue
+            if check_macos_files(TPL) or TPL.startswith("."):
+                continue
+
             binary_file_folder = os.path.join(cat_folder, TPL)
             for bf in os.listdir(binary_file_folder):
-                if check_macos_files(bf): continue
+                if check_macos_files(bf):
+                    continue
                 if bf.endswith('.jar'):
                     num_jar += 1
-                    print("%s >> %s >> %s" % (TPL_cat, TPL, bf))
                     statistics[TPL_cat] += 1
-                    break
                 elif bf.endswith('.aar'):
-                    num_arr += 1
-                    print("%s >> %s >> %s" % (TPL_cat, TPL, bf))
+                    num_aar += 1
                     statistics[TPL_cat] += 1
+                if bf.endswith('.jar') or bf.endswith('.aar'):
+                    print("%s >> %s >> %s" % (TPL_cat, TPL, bf))
                     break
-    #
-    print(' ============================== ')
-    print('The dataset contains %d distinct TPL binary files '
-          'and includes %d *.jar files and %d *.aar files: ' % (num_jar + num_arr, num_jar, num_arr))
-    #
-    for tpl_cat in statistics:
-        print('\t%s: %d' % (tpl_cat, statistics[tpl_cat]))
 
-if __name__ == '__main__':
-    TPL_binaryfiles_folder = '/ATPChecker/dataset/TPL_data'
-    main(TPL_binaryfiles_folder)
+    summary = {
+        'statistics': statistics,
+        'total_binary_files': num_jar + num_aar,
+        'num_jar': num_jar,
+        'num_aar': num_aar,
+    }
+
+    print(' ============================== ')
+    print(f'The dataset contains {summary["total_binary_files"]} distinct TPL binary files '
+          f'and includes {summary["num_jar"]} *.jar files and {summary["num_aar"]} *.aar files.')
+
+    for tpl_cat, count in statistics.items():
+        print(f'\t{tpl_cat}: {count}')
+
+    return summary
+
+# Example of how you would use it in another script:
+# summary = analyze_tpl_binary_files("/path/to/TPL_binaryfiles_folder")
+# print(summary)
--- script_runner.py
+++ script_runner.py
@@ -1,14 +1,17 @@
 # Part 1 imports
 from Part1_dataset.Step1_1_get_TPL_list_info import tpl_statistics
 from Part1_dataset.Step1_2_get_TPL_privacy_policy_files import analyze_tpl_privacy_policies
+from Part1_dataset.Step1_3_get_TPL_binaryfiles_info import analyze_tpl_binary_files
 
 # Define the path to the TPL list folder you want to analyze
 tpl_list_folder_path = "dataset/TPL_PP"
+tpl_data_folder_path = "dataset/TPL_data"
 
 # Call the tpl_statistics function with the path to your TPL list folder
 # The function will print the statistics and also return them as a string
 tpl_statistics_output = tpl_statistics(tpl_list_folder_path)
 analyze_tpl_privacy_policies_output = analyze_tpl_privacy_policies(tpl_list_folder_path)
+analyze_tpl_binaryfiles_output = analyze_tpl_binary_files(tpl_data_folder_path)
 
 # Print outputs
 print("Returned tpl_statistics_output:")
@@ -16,4 +19,7 @@ print(tpl_statistics_output)
 
 print("\nReturned analyze_tpl_privacy_policies_output:")
 print(analyze_tpl_privacy_policies_output)
+
+print("\nReturned analyze_tpl_binaryfiles_output:")
+print(analyze_tpl_binaryfiles_output)
 
