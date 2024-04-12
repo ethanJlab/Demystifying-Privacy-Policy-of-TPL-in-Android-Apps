8f35891ac87d1ceafdf9b8491bc29953d02455ee
ethanJlab <ethanjanicki89@gmail.com>
2024-04-12 19:30:16 -0400

    WIP GUI

--- gui_interface.py
+++ gui_interface.py
@@ -1,5 +1,7 @@
 import gradio as gr
+from gradio_pdf import PDF
 
 with gr.Blocks() as demo:
   dataset_file = gr.File(label = "Upload Dataset File Here")
+  display= gr.Textbox(label="Question"), PDF(dataset_file)
 demo.launch()
