a28f7bfbd1304641088a638b29059d4f8d67a4c0
ethanJlab <ethanjanicki89@gmail.com>
2024-04-12 19:56:11 -0400

    Update GUI interface to include PDF output functionality

--- gui_interface.py
+++ gui_interface.py
@@ -2,6 +2,8 @@ import gradio as gr
 from gradio_pdf import PDF
 
 with gr.Blocks() as demo:
-  dataset_file = gr.File(label = "Upload Dataset File Here")
-  display= gr.Textbox(label="Question"), PDF(dataset_file)
+  dataset_file = gr.File(label = "Upload Dataset File Here", )
+
+  output_PDF_path = "./Fig5.pdf"
+  PDF(output_PDF_path, label = "PDF")
 demo.launch()
